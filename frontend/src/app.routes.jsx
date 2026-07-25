a1ee3f888f55f2dc0206f44a15e8d060fa3e44d0
“vaibhavrao74” <vrlx1975@gamil.com>
2026-07-25 18:23:52 +0530

    Fix import case for Login page

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router";
-import Login from "./features/auth/pages/Login";
+import Login from "./features/auth/pages/login";
 import Register from "./features/auth/pages/Register";
 import Protected from "./features/auth/components/Protected";
 import Home from "./features/interview/pages/Home";
