b4006fc24d8e3a3eaf6505e51d73651c3efefc4c
“vaibhavrao74” <vrlx1975@gamil.com>
2026-07-25 17:28:39 +0530

    :wqConfigure frontend API URL and update gitignore
    :wq#:wq

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
 connectToDB()
+<<<<<<< HEAD
 const PORT= process.env.PORT || 3000;
+=======
+const PORT = process.env.PORT || 3000
+>>>>>>> 364be36 (Configure frontend API URL and update gitignore)
 
 
 app.listen(PORT, () => {
